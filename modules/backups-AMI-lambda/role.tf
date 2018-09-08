3735301dd079279e3997b264848474b7f8a90a5f
Artem <Sorok7@gmail.com>
2018-09-08 06:27:46 +0200

    add roles

---
+++ modules/backups-AMI-lambda/role.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_role" "role_backup" {
+  name               = "${var.name_tag}-${var.environment_tag}-slave-role"
+  assume_role_policy = "${file("${path.module}/files/ec2-trust.json")}"
+}
+
+resource "aws_iam_policy" "role_backup" {
+  name        = "${var.name_tag}-${var.environment_tag}-slave-cw-policy"
+  description = "CloudWatch Policy for ${var.name_tag} slave"
+  policy      = "${file("${path.module}/files/cw-policy-slave.json")}"
+}
+
+resource "aws_iam_role_policy_attachment" "policy_to_role" {
+  role       = "${aws_iam_role.role_backup.name}"
+  policy_arn = "${aws_iam_policy.role_backup.arn}"
+}˝
